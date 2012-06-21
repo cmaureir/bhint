76716196d91a285275bc3ac63a5eb28cb842caaf
Cristian Maureira Fredes <cmaureir@csrg.inf.utfsm.cl>
2012-06-21 14:38:15 +0200

    Cleaning bhint original to a more slim version

--- gpu/run-test.sh
+++ gpu/run-test.sh
@@ -1,4 +1,4 @@
 make clean all && \
-./bhint i ../data/model1.dat -5.e2 0 out && \
-./bhint d outmodel1.dat_1.00e-01_80_-5.0e+02.bhint_1.dump > dump1
-./bhint d outmodel1.dat_1.00e-01_80_-5.0e+02.bhint_2.dump > dump2
+./bhint i ../data/model1.dat -5 0 out > pos && \
+./bhint d outmodel1.dat_1.00e-01_80_-5.0e+00.bhint_1.dump > dump1
+./bhint d outmodel1.dat_1.00e-01_80_-5.0e+00.bhint_2.dump > dump2
